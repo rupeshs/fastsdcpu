f615ebaef402d56904558b971fff4e93b84cc316
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-10-21 17:52:28 +0530

    Fixed to handle path with space in batch,shell scripts

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-%~dp0tools\windows\micromamba.exe -r %~dp0env create -y -f %~dp0environment.yml
+"%~dp0tools\windows\micromamba.exe" -r "%~dp0env" create -y -f "%~dp0environment.yml"
 echo Env installation completed.
 pause
--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting fastsdcpu
-cd %~dp0env\condabin\
+cd "%~dp0env\condabin\"
 call activate.bat
-micromamba activate %~dp0env\envs\fastsd-env && python %~dp0main.py
+micromamba activate "%~dp0env\envs\fastsd-env" && python "%~dp0main.py"
 
