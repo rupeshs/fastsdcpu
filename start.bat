8b0094705c0f2db22c63b7f8126edb684b7df882
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-10-22 22:10:00 +0530

    Add OpenVINO support

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting fastsdcpu
 cd "%~dp0env\condabin\"
+set PATH=%PATH%;%~dp0env\envs\fastsd-env\Lib\site-packages\openvino\libs
 call activate.bat
 micromamba activate "%~dp0env\envs\fastsd-env" && python "%~dp0main.py"
-
