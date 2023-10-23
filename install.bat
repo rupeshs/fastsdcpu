5fadbb597920ab39e60c6e424bd1af2d048a8a2d
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-10-23 17:36:49 +0530

    Added venv support,removed micromamba

--- install.bat
+++ install.bat
@@ -1,6 +1,25 @@
-"%~dp0tools\windows\micromamba.exe" -r "%~dp0env" create -y -f "%~dp0environment.yml"
-cd "%~dp0env\condabin\"
-call activate.bat
-call micromamba activate "%~dp0env\envs\fastsd-env" && pip install -r "%~dp0requirements.txt"
-echo Env installation completed.
+
+@echo off
+setlocal
+
+set "PYTHON_COMMAND=python"
+
+where python >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Error: Python not found, please install Python 3.8 or higher.
+        exit /b 1
+    ) 
+
+echo Found %PYTHON_COMMAND% command
+
+:check_python_version
+for /f "tokens=2" %%I in ('%PYTHON_COMMAND% --version 2^>^&1') do (
+    set "python_version=%%I"
+)
+
+echo Python version: %python_version%
+
+%PYTHON_COMMAND% -m venv "%~dp0env" 
+call "%~dp0env\Scripts\activate.bat" && pip install -r "%~dp0requirements.txt"
+echo FastSD CPU env installation completed.
 pause
--- start.bat
+++ start.bat
@@ -1,6 +1,25 @@
+
 @echo off
-echo Starting fastsdcpu
-cd "%~dp0env\condabin\"
+setlocal
+echo Starting fastsdcpu...
+
+set "PYTHON_COMMAND=python"
+
+where python >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Error: Python not found, please install Python 3.8 or higher.
+        exit /b 1
+    ) 
+
+echo Found %PYTHON_COMMAND% command
+
+:check_python_version
+for /f "tokens=2" %%I in ('%PYTHON_COMMAND% --version 2^>^&1') do (
+    set "python_version=%%I"
+)
+
+echo Python version: %python_version%
+
+
 set PATH=%PATH%;%~dp0env\envs\fastsd-env\Lib\site-packages\openvino\libs
-call activate.bat
-micromamba activate "%~dp0env\envs\fastsd-env" && python "%~dp0main.py"
+%PYTHON_COMMAND% -m venv "%~dp0env"  && python "%~dp0main.py"
