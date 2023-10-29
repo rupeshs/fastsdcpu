8d3ef941b7b5777eb7d09ec9ff1a90df4c41ca4a
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-10-29 10:39:11 +0530

    Added and updated shell scripts

--- install-gpu-cuda.sh
+++ install-gpu-cuda.sh
@@ -24,7 +24,7 @@ BASEDIR=$(pwd)
 $PYTHON_COMMAND -m venv "$BASEDIR/env"
 # shellcheck disable=SC1091
 source "$BASEDIR/env/bin/activate"
-pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/cu117
 pip install -r "$BASEDIR/requirements.txt"
 chmod +x "start.sh"
 
