297d3118ed030b138c57ec4c5f0bc65037fd6c55
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-11-14 06:51:55 +0530

    monkey patching gradio for fast response

--- src/frontend/webui/realtime_ui.py
+++ src/frontend/webui/realtime_ui.py
@@ -7,6 +7,23 @@ lcm_lora = LCMLora(
     base_model_id="Lykon/dreamshaper-7",
     lcm_lora_id="latent-consistency/lcm-lora-sdv1-5",
 )
+import numpy as np
+import cv2
+from cv2 import imencode
+import base64
+
+
+# https://github.com/gradio-app/gradio/issues/2635#issuecomment-1423531319
+def encode_pil_to_base64_new(pil_image):
+    print("using new encoding method")
+    image_arr = np.asarray(pil_image)[:, :, ::-1]
+    _, byte_data = imencode(".png", image_arr)
+    base64_data = base64.b64encode(byte_data)
+    base64_string_opencv = base64_data.decode("utf-8")
+    return "data:image/png;base64," + base64_string_opencv
+
+
+gr.processing_utils.encode_pil_to_base64 = encode_pil_to_base64_new
 
 
 def predict(
