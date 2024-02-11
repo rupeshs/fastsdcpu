f87b1e9ef154639a1289d1d52b3b3c9bc508b43b
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2024-02-11 08:11:14 +0530

    Fix LCM-LoRA offline error

--- src/backend/pipelines/lcm_lora.py
+++ src/backend/pipelines/lcm_lora.py
@@ -13,9 +13,13 @@ def get_lcm_lora_pipeline(
         torch_dtype=torch_data_type,
         local_files_only=use_local_model,
     )
+    kwargs = {
+        "local_files_only": use_local_model,
+        "weight_name": "pytorch_lora_weights.safetensors",
+    }
     pipeline.load_lora_weights(
         lcm_lora_id,
-        local_files_only=use_local_model,
+        **kwargs,
     )
     if "lcm" in lcm_lora_id.lower():
         print("LCM LoRA model detected so using recommended LCMScheduler")
