519694ea2ce2615185f59b8463fc7e70e95d9168
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2025-04-17 18:55:28 +0530

    Add scripts for linux

--- start-mcpserver.sh
+++ start-mcpserver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo Starting FastSD CPU env installation...
+echo Starting FastSD CPU please wait...
 set -e
 PYTHON_COMMAND="python3"
 
@@ -20,20 +20,6 @@ python_version=$($PYTHON_COMMAND --version 2>&1 | awk '{print $2}')
 echo "Python version : $python_version"
 
 BASEDIR=$(pwd)
-
-$PYTHON_COMMAND -m venv "$BASEDIR/env"
 # shellcheck disable=SC1091
 source "$BASEDIR/env/bin/activate"
-pip install torch --index-url https://download.pytorch.org/whl/cpu
-if [[ "$1" == "--disable-gui" ]]; then
-    #! For termux , we don't need Qt based GUI
-    packages="$(grep -v "^ *#\|^PyQt5" requirements.txt | grep .)" 
-    # shellcheck disable=SC2086
-    pip install $packages
-else
-    pip install -r "$BASEDIR/requirements.txt"
-fi
-
-chmod +x "start.sh"
-chmod +x "start-webui.sh"
-read -n1 -r -p "FastSD CPU installation completed,press any key to continue..." key
+$PYTHON_COMMAND src/app.py --mcp
